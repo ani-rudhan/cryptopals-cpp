6b3f98c6b249f0b8e22f70ced9546d1eb40f586d
Anirudhan Madhavan <anirudhan.in@gmail.com>
2025-12-12 16:34:49 +0000

    fixedXOR init version

---
+++ Set-1/Challenge-2/fixedXOR.cpp
@@ -0,0 +1,52 @@
+/*
+    Set 1  Challenge 2:
+
+    Fixed XOR
+    Write a function that takes two equal-length buffers and produces their XOR combination.
+
+    If your function works properly, then when you feed it the string:
+
+    1c0111001f010100061a024b53535009181c
+    ... after hex decoding, and when XOR'd against:
+
+    686974207468652062756c6c277320657965
+    ... should produce:
+
+    746865206b696420646f6e277420706c6179
+*/
+
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <sstream>
+
+namespace
+{
+    const std::string stringA = "1c0111001f010100061a024b53535009181c";
+    const std::string stringB = "686974207468652062756c6c277320657965";
+}
+
+std::string fixedLengthXOR(const std::string stringA, const std::string stringB)
+{
+    std::string xorResult = "";
+
+    for (auto itrA = stringA.begin(), itrB = stringB.begin(); ;itrA++,itrB++)
+    {
+        if ((itrA == stringA.end()) && (itrB == stringB.end()))
+        {
+            break;
+        }
+
+        auto binA = std::bitset<4>(std::stoul(std::string(1, *itrA), nullptr, 16));
+        auto binB = std::bitset<4>(std::stoul(std::string(1, *itrB), nullptr, 16));
+        auto binC = binA ^ binB;
+        std::stringstream ss;
+        ss << std::hex << binC.to_ulong();
+        xorResult += ss.str();
+    }
+}
+
+int main()
+{
+    std::cout << fixedLengthXOR(stringA, stringB) << std::endl;
+}
